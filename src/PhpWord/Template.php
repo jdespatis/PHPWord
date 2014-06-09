c66dc62048c86de864c382273a4c1dbcd48ec7a6
Jerome Despatis <jerome.despatis@source-rh.com>
2014-06-09 12:24:22 +0200

    HACK implementation of https://github.com/PHPOffice/PHPWord/issues/260

--- src/PhpWord/Template.php
+++ src/PhpWord/Template.php
@@ -130,6 +130,27 @@ class Template
     }
 
     /**
+     * Set a new image
+     *
+     * @param string $search
+     * @param string $replace
+     */
+    public function setImageValue($search, $replace)
+    {
+        // Sanity check
+        if (!file_exists($replace))
+        {
+            return;
+        }
+
+        // Delete current image
+        $this->zipClass->deleteName('word/media/' . $search);
+
+        // Add a new one
+        $this->zipClass->addFile($replace, 'word/media/' . $search);
+    }
+
+    /**
      * Set a Template value
      *
      * @param mixed $search
